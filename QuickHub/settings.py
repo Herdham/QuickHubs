fb17aceaa22b0456226f1af6633522351556d4bd
QuickHub <musaadambabatunde@gmail.com>
2024-06-23 12:42:09 -0700

    more changes

--- QuickHub/settings.py
+++ QuickHub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p!9(#c=m0tqc--254&qn2d3*z$w*ok4vat#lj5drk-2o$j4apv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'quickhubs.onrender.com']
 
 
 # Application definition
