fb450db62b0834043e34460100b0f1d946d9dca1
QuickHub <musaadambabatunde@gmail.com>
2024-06-24 15:26:03 -0700

    more changes

--- QuickHub/settings.py
+++ QuickHub/settings.py
@@ -123,7 +123,6 @@ STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATICFILES_DIRS = [BASE_DIR / 'mystaticfiles']
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
