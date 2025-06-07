f6c936e3a9740b79a0edd7d0751d817008c25e43
Herdham <musaadambabatunde@gmail.com>
2025-06-07 08:24:28 -0700

    Django project push

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
-#!/usr/bin/env bash
-# exit on error
+#!/bin/bash
+set -e
 
-set -o errexit
+# Update apt and install dependencies to build and run MariaDB Connector/C
+apt-get update && apt-get install -y wget build-essential cmake
 
+# Remove old mariadb connector if installed (optional, but safer)
+apt-get remove -y libmariadb3 libmariadb-dev || true
+
+# Download and install MariaDB Connector/C >= 3.3.1
+wget https://downloads.mariadb.com/Connectors/c/connector-c-3.3.3/mariadb-connector-c_3.3.3-1_amd64.deb
+dpkg -i mariadb-connector-c_3.3.3-1_amd64.deb
+
+# Optional: fix broken dependencies if any
+apt-get install -f -y
+
+# Now install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
-
-python manage.py collectstatic --no-input
-python manage.py migrate
